282d03132677c167c41f73421bbdf76ae7e240da
karrarjasim <k.jasim@s-gate.iq>
2022-08-26 00:43:19 +0300

    add how to play dialog

--- app/src/main/java/com/devfalah/quiz/MainActivity.kt
+++ app/src/main/java/com/devfalah/quiz/MainActivity.kt
@@ -2,10 +2,24 @@ package com.devfalah.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.devfalah.quiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        replaceFragment(homeFragment())
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
     }
 }
