cd61f7f62774d31437844cd9c47311e87cfa79a2
devsadeq <dev.sadeq@gmail.com>
2022-08-27 00:10:03 +0300

    complete refactor ui

--- app/src/main/java/com/devfalah/quiz/ui/MainActivity.kt
+++ app/src/main/java/com/devfalah/quiz/ui/MainActivity.kt
@@ -2,19 +2,7 @@ package com.devfalah.quiz.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import com.devfalah.quiz.R
 import com.devfalah.quiz.databinding.ActivityMainBinding
-
-//
-//class MainActivity : AppCompatActivity() {
-//    lateinit var binding: ActivityMainBinding
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        binding = ActivityMainBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
-//    }
-//}
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -22,15 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        replaceFragment(homeFragment())
     }
 
-    private fun replaceFragment(fragment: Fragment) {
-
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.fragment_container, fragment)
-        fragmentTransaction.commit()
-    }
 }
