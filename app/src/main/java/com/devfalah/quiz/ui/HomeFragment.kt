cf0953e024c188b2fc3a4630e7aea2dfeb0edc18
moh05hus <92690501+moh05hus@users.noreply.github.com>
2022-08-26 20:47:12 +0300

    add navigation on and on click start play button wall go to mcq fragment

--- app/src/main/java/com/devfalah/quiz/ui/HomeFragment.kt
+++ app/src/main/java/com/devfalah/quiz/ui/HomeFragment.kt
@@ -4,17 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.devfalah.quiz.R
+import com.devfalah.quiz.databinding.FragmentHomeBinding
 
-class homeFragment : Fragment(R.layout.fragment_home) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+class HomeFragment : Fragment(R.layout.fragment_home) {
+lateinit var binding : FragmentHomeBinding
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    binding.startPlayButton.setOnClickListener { v ->
+
+        Navigation.findNavController(v).navigate(R.id.action_homeFragment_to_mcqFragment)
+
+    }
+
+    }
+
 
 }
