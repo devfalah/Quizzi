2902fac3e713cccae244c025badbedbe1f705858
devsadeq <dev.sadeq@gmail.com>
2022-08-26 22:05:00 +0300

    refactor home text style

--- app/src/main/java/com/devfalah/quiz/ui/MainActivity.kt
+++ app/src/main/java/com/devfalah/quiz/ui/MainActivity.kt
@@ -2,7 +2,19 @@ package com.devfalah.quiz.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.devfalah.quiz.R
 import com.devfalah.quiz.databinding.ActivityMainBinding
+
+//
+//class MainActivity : AppCompatActivity() {
+//    lateinit var binding: ActivityMainBinding
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//    }
+//}
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -10,6 +22,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        replaceFragment(homeFragment())
     }
 
+    private fun replaceFragment(fragment: Fragment) {
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
+    }
 }
