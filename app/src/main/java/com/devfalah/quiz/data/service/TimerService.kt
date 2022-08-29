0367e5520be57c7158ac12a1383e2ac44376b318
devsadeq <dev.sadeq@gmail.com>
2022-08-29 19:37:13 +0300

    add timer dispose

--- app/src/main/java/com/devfalah/quiz/data/service/TimerService.kt
+++ app/src/main/java/com/devfalah/quiz/data/service/TimerService.kt
@@ -1,13 +1,16 @@
 package com.devfalah.quiz.data.service
 
+import com.devfalah.quiz.utilities.add
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
 
 
 abstract class TimerService(private val endValue: Long, private val timeUnit: TimeUnit) {
+    lateinit var compositeDisposable: CompositeDisposable
 
     abstract fun onTick(tickValue: Long)
 
@@ -23,6 +26,7 @@ abstract class TimerService(private val endValue: Long, private val timeUnit: Ti
             endValue - i
         }.subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread()).subscribe(::onNext, ::onError, ::onComplete)
+            .add(compositeDisposable)
     }
 
     private fun onNext(long: Long) {
@@ -37,4 +41,10 @@ abstract class TimerService(private val endValue: Long, private val timeUnit: Ti
         onFinish()
     }
 
+    fun dispose() {
+        compositeDisposable.dispose()
+    }
+
 }
+
+
--- app/src/main/java/com/devfalah/quiz/utilities/Extensions.kt
+++ app/src/main/java/com/devfalah/quiz/utilities/Extensions.kt
@@ -3,14 +3,20 @@ package com.devfalah.quiz.utilities
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.schedulers.Schedulers
 
 fun <E> MutableList<E>.addAll(elements: List<E?>) {
-    elements.forEach{
+    elements.forEach {
         this.add(it!!)
     }
 }
 
-fun <T> Observable<T>.observeOnMainThread(){
+fun <T> Observable<T>.observeOnMainThread() {
     observeOn(AndroidSchedulers.mainThread())
+}
+
+fun Disposable.add(compositeDisposable: CompositeDisposable) {
+    compositeDisposable.add(this)
 }
