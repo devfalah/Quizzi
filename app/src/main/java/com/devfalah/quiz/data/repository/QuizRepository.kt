061daf572cd0a4f0371c9d67c274bc052bf8a821
Falah Hassan <devfalahhassan@gmail.com>
2022-08-31 22:23:06 -0700

    code refactor

--- app/src/main/java/com/devfalah/quiz/data/repository/QuizRepository.kt
+++ app/src/main/java/com/devfalah/quiz/data/repository/QuizRepository.kt
@@ -1,7 +1,7 @@
 package com.devfalah.quiz.data.repository
 
 import com.devfalah.quiz.data.model.QuizResponse
-import com.devfalah.quiz.utilities.McqDifficulty
+import com.devfalah.quiz.utilities.enums.McqDifficulty
 import com.devfalah.quiz.utilities.State
 import io.reactivex.rxjava3.core.Single
 
--- app/src/main/java/com/devfalah/quiz/ui/result/ResultFragment.kt
+++ app/src/main/java/com/devfalah/quiz/ui/result/ResultFragment.kt
@@ -4,12 +4,11 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.viewModelScope
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
 import com.devfalah.quiz.R
 import com.devfalah.quiz.databinding.FragmentResultBinding
 import com.devfalah.quiz.ui.base.BaseFragment
+import com.devfalah.quiz.utilities.goToFragment
 
 
 class ResultFragment : BaseFragment<FragmentResultBinding>() {
@@ -26,10 +25,14 @@ class ResultFragment : BaseFragment<FragmentResultBinding>() {
             viewModel = this@ResultFragment.viewModel
         }
         viewModel.setResult(args.score, args.correctAnswersCount)
+        addCallbacks()
 
-        binding?.homeButton?.setOnClickListener { view ->
-            val action = ResultFragmentDirections.actionResultFragmentToHomeFragment()
-            view.findNavController().navigate(action)
+    }
+
+
+    private fun addCallbacks() {
+        binding!!.homeButton.setOnClickListener { view ->
+            view.goToFragment(ResultFragmentDirections.actionResultFragmentToHomeFragment())
         }
     }
 }
